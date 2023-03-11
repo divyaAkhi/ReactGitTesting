4ba3511eab905da99d5e2a07ccc87e53f48ce53c
Divya Rekha <divyatulluri173@gmail.com>
2023-03-11 10:50:41 +0530

    Hello All Commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome To React.... <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +19,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <p>Welcome React Project</p>
+        
       </header>
     </div>
   );
